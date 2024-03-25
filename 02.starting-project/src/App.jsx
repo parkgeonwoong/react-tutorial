3d60bea1f5d5f55c62c1edbd8be3f63bfeda949f
parkgeonwoong <hk77kh@gmail.com>
2024-03-25 09:07:42 +0900

    feat : 동적 이미지 및 Props 설정

--- 02.starting-project/src/App.jsx
+++ 02.starting-project/src/App.jsx
@@ -1,16 +1,40 @@
+import coreConcepts from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
+  const description = reactDescriptions[getRandomInt(2)];
   return (
     <div>
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={coreConcepts} alt="Stylized atom" />
         <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
+        <p>{description} React concepts you will need for almost any app you are going to build!</p>
       </header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((concept) => (
+              <CoreConcept key={concept.title} {...concept} />
+            ))}
+          </ul>
+        </section>
       </main>
     </div>
   );
