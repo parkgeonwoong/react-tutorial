7740d5116013eeff9f88f0fb35b94b0d9c11705f
parkgeonwoong <hk77kh@gmail.com>
2024-03-27 22:43:05 +0900

    feat : children props & 컴포넌트 분리

--- 02.starting-project/src/App.jsx
+++ 02.starting-project/src/App.jsx
@@ -1,31 +1,12 @@
-import coreConcepts from "./assets/react-core-concepts.png";
+import CoreConcept from "./components/CoreConcept";
+import Header from "./components/Header/Header";
+import TabButton from "./components/TabButton";
 import { CORE_CONCEPTS } from "./data";
 
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
-
 function App() {
-  const description = reactDescriptions[getRandomInt(2)];
   return (
     <div>
-      <header>
-        <img src={coreConcepts} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>{description} React concepts you will need for almost any app you are going to build!</p>
-      </header>
+      <Header />
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
@@ -34,6 +15,15 @@ function App() {
               <CoreConcept key={concept.title} {...concept} />
             ))}
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
