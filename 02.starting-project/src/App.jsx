798f90987a2396476bcd9d7cb02bf537abbe0fca
parkgeonwoong <hk77kh@gmail.com>
2024-04-28 18:24:48 +0900

    feat : [tic] 게임 세팅 준비

--- 02.starting-project/src/App.jsx
+++ 02.starting-project/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -69,7 +69,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
