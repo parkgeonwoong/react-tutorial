51fca1a8aa9719eb533316746e151e87ffc1b56e
parkgeonwoong <hk77kh@gmail.com>
2024-05-15 23:20:27 +0900

    feat : 계산된 값 끌어올리고, 새로운 값 파생하기

--- 03.tic-tac/src/components/GameBoard.jsx
+++ 03.tic-tac/src/components/GameBoard.jsx
@@ -1,35 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  // 리액트에서 사고하고 작업하는 방식 => 제어하는 상태의 수는 최소화, 각 상태에서 가능한 많은 정보와 많은 값 파생하는 것
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
-  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  //   function handleSelectSquare(rowIndex, colIndex) {
-  //     setGameBoard((prevGameBoard) => {
-  //       const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
-  //       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //       return updatedBoard;
-  //     });
-
-  //     onSelectSquare();
-  //   }
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
