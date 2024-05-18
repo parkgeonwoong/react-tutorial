4b72d737576c4ca2fde1a5116f031841b7df5a34
parkgeonwoong <hk77kh@gmail.com>
2024-05-18 17:32:00 +0900

    init : Practice Project

---
+++ 04.Essentials Practice/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from "./assets/investment-calculator-logo.png";
+
+function App() {
+  return (
+    <>
+      <div id="header">
+        <h1>React Investment Calculator</h1>
+        <img src={logo} />
+      </div>
+    </>
+  );
+}
+
+export default App;
