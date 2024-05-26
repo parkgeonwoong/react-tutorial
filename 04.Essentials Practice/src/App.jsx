ce3bcc7ff721c761ecf8af898c6224a8111d070a
parkgeonwoong <hk77kh@gmail.com>
2024-05-26 21:55:24 +0900

    feat : 조건적 콘텐츠 출력

--- 04.Essentials Practice/src/App.jsx
+++ 04.Essentials Practice/src/App.jsx
@@ -11,6 +11,8 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -23,7 +25,8 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && <p className="center">Please enter a valid duration (at least 1 year).</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
